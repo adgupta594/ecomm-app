b63ced6997baf7fe6426f394e69d3b3fa4ba1cdd
Aditya Gupta <adgupta594@gmail.com>
2023-05-31 15:06:47 +0530

    Added Routing to shop page and mad a basic structure of the shop page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import "./App.css";
+
+import { Route, Routes } from "react-router-dom";
 import Homepage from "./pages/homepage/hompage.component";
+import Shop from "./pages/shoppage/shop.component";
 
 function App() {
   return (
     <div>
-      <Homepage />
+      <Routes>
+        <Route exact path="/" Component={Homepage} />
+        <Route path="/shop" Component={Shop} />
+      </Routes>
     </div>
   );
 }
