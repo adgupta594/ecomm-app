b9fb64868b0d45c12a73c4f3316a371ae01c8de3
Aditya Gupta <adgupta594@gmail.com>
2023-06-28 17:00:29 +0530

    Add to cart action created

--- src/components/custom-button/custom-button.component.jsx
+++ src/components/custom-button/custom-button.component.jsx
@@ -2,10 +2,12 @@ import React from "react";
 
 import "./custom-button.style.scss";
 
-const CustomButton = ({ children, isGoogleSignIn, ...others }) => {
+const CustomButton = ({ children, isGoogleSignIn, inverted, ...others }) => {
   return (
     <button
-      className={`${isGoogleSignIn ? "google-sign-in" : ""} custom-button `}
+      className={`${inverted ? "inverted" : ""} ${
+        isGoogleSignIn ? "google-sign-in" : ""
+      } custom-button `}
       {...others}
     >
       {children}
